c4f34de3f504fc3f14a2a7b04bf68cddf4dc502c
+ <+githinjiadreen27@gmail.com>
2025-09-22 10:09:27 +0300

    Building a GET API

--- server/app.py
+++ server/app.py
@@ -14,25 +14,65 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
+# GET /bakeries
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = Bakery.query.all()
+    response = make_response(
+        jsonify([bakery.to_dict() for bakery in all_bakeries]),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
+
+# GET /bakeries/<int:id>
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    response = make_response(
+        jsonify(bakery.to_dict(rules=('-baked_goods.bakery',))),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
+
+# GET /baked_goods/by_price
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    response = make_response(
+        jsonify([bg.to_dict(rules=('-bakery.baked_goods',)) for bg in goods]),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
+
+# GET /baked_goods/most_expensive
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        response = make_response(
+            jsonify(baked_good.to_dict(rules=('-bakery.baked_goods',))),
+            200
+        )
+    else:
+        response = make_response(
+            jsonify({'error': 'No baked goods found'}),
+            404
+        )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
